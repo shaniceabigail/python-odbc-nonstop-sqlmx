3cd4ed9b2aa8f9ed141a914eb02ebce4453f8651
Shanice Abigail <shanice.abigail.work@gmail.com>
2021-03-25 17:19:08 +0800

    added python file to access data from nonstop sql

---
+++ python-odbc-nonstop.py
@@ -0,0 +1,18 @@
+import pyodbc
+
+conn = pyodbc.connect('DSN=[CATALOG NAME];UID=[USER];PWD=[PASSWORD]')
+
+conn.setdecoding(pyodbc.SQL_CHAR, encoding='iso-8859-1')
+conn.setdecoding(pyodbc.SQL_WCHAR, encoding='iso-8859-1')
+conn.setencoding(encoding='iso-8859-1')
+
+cursor = conn.cursor()
+cursor.execute('INSERT INTO CATALOG.SCHEMA.TABLE VALUES (VALUE1, VALUE2)')
+
+# makes sure that insert statement is a committed transaction in NonStop SQL database
+conn.commit()
+
+# prints table to make sure that data was updated
+cursor.execute('SELECT * FROM CATALOG.SCHEMA.TABLE')
+for row in cursor:
+    print(row)
