b1b7d73dc90b118707955639ba924a85066f6e44
Shanice Abigail <shanice.abigail.work@gmail.com>
2021-03-30 13:55:28 +0800

    added clarifying comment for sql/mx

--- python-odbc-nonstop.py
+++ python-odbc-nonstop.py
@@ -2,6 +2,7 @@ import pyodbc
 
 conn = pyodbc.connect('DSN=[DATA SOURCE NAME];UID=[USER];PWD=[PASSWORD]')
 
+# Encoding and decoding specific to NonStop SQL/MX
 conn.setdecoding(pyodbc.SQL_CHAR, encoding='iso-8859-1')
 conn.setdecoding(pyodbc.SQL_WCHAR, encoding='iso-8859-1')
 conn.setencoding(encoding='iso-8859-1')
@@ -9,7 +10,7 @@ conn.setencoding(encoding='iso-8859-1')
 cursor = conn.cursor()
 cursor.execute('INSERT INTO CATALOG.SCHEMA.TABLE VALUES (VALUE1, VALUE2)')
 
-# makes sure that insert statement is a committed transaction in NonStop SQL database
+# makes sure that insert statement is a committed transaction in NonStop SQL/MX database
 conn.commit()
 
 # prints table to make sure that data was updated
